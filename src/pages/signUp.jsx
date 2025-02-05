ff4ac029353be31312d3e83ee1aea21caf98f6e4
sitanshu01 <nayansitanshu@gmail.com>
2025-02-06 01:06:53 +0530

    changes added

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -5,10 +5,10 @@ export const SignUp = () => {
   return (
     <div className="py-4 px-12 flex justify-center items-center min-h-screen">
       <div className="w-3/4 h-full m-10 hidden md:block ">
-        <img className="object-cover  absolute bottom-0 left-20 md:w-100 md:h-120"
+        <img className="object-cover absolute bottom-0 left-20 md:w-100 md:h-120"
         src="../../../rb_25585 2.png"/>
       </div>
-      <div className="bg-slate-50/20 w-full m-10 rounded-xl py-8 px-10 backdrop-blur-[3px]">
+      <div className="bg-slate-50/20 w-1/2 rounded-xl py-8 px-10 backdrop-blur-[3px] mr-30">
         <LinearGradientText text={"Sign Up"} />
         <div className="flex flex-col gap-3 mt-10">
           <Input type="text" placeholder="Email" />
