871ea13235b440c4d988efda0bb12a1e15738cb1
sitanshu01 <nayansitanshu@gmail.com>
2025-02-07 01:51:16 +0530

    added responsives and updated the page

--- src/component/ui/button.jsx
+++ src/component/ui/button.jsx
@@ -1,5 +1,3 @@
-import { UpRightArrow } from "../../icons/up-rightArrow.jsx";
-
 const variantStyles = {
   main: "bg-[#242C31] md:text-lg  text-[#f9f9f9] font-medium rounded border-teal-50/10 border hover:bg-teal-900 cursor-pointer transition-all",
 };
@@ -8,14 +6,15 @@ const sizeStyles = {
   sm: "py-1 px-2 text-sm",
   md: "py-1 px-3 text-sm",
   lg: "py-1 px-3 text-lg",
+  xl: "py-1 px-4 text-xl",
+  dxl: "py-2 px-4 text-2xl",
+  fxl: "w-full px-[0%] py-1 md:text-2xl xl",
 };
 
-export const Button = ({ variant, text, size, endIcon }) => {
+export const Button = ({ variant, text, size }) => {
   return (
     <button className={`${variantStyles[variant]} ${sizeStyles[size]}`}>
-      <div className="flex gap-2 items-center">
-        {text} {endIcon && <UpRightArrow />}
-      </div>
+        {text} 
     </button>
   );
 };
--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -3,26 +3,35 @@ import { LinearGradientText } from "../component/ui/linearGradientText";
 import { Button } from "../component/ui/button";
 export const SignIn = () => {
   return (
-    <div className="py-4 px-12 flex justify-center items-center min-h-screen">
-      <div className="sm:hidden">
-        <img />
-      </div>
-      <div className="bg-slate-50/20 w-full  rounded-xl py-8 px-10 backdrop-blur-[3px]">
-        <LinearGradientText text={"Sign In"} />
-        <div className="flex flex-col gap-3 mt-10">
-          <Input type="text" placeholder="Email" />
-          <Input type="password" placeholder="password" />
+    <div className="flex flex-col justify-center min-h-screen">
+        <div className="bg-cover absolute top-0 md:left-[40%] left-29 m-10 h-4 flex justify-center items-center">
+          <img src="../public/prodyWhite.webp" alt="logo" className="object-cover h-16"/>
+          <h1 className="text-[#f9f9f9] font-bold text-lg md:text-2xl m-3 cursor-pointer">
+              Abhedya
+            </h1>
+        </div>   
+      <div className="flex justify-center items-center ">     
+        <div className="bg-slate-50/20 w-3/4 md:w-130 rounded-xl py-8 px-10 backdrop-blur-[3px] md:ml-30 mr-0">
+          <LinearGradientText text={"Sign In"} />
+          <div className="flex flex-col gap-3 mt-10">
+            <Input type="text" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+          </div>
+          <div className="mt-7 flex justify-center items-center">
+            <Button
+              text={"Sign In"}
+              variant={"main"}
+              size={"fxl"}
+            />
+          </div>
+
+        <p className="text-[#f9f9f9] text-lg md:text-lg m-3 cursor-pointer" >New to Abhedya <a className="text-blue-500 md:text-lg font-bold underline">Sign Up</a></p>
         </div>
-        <div className="mt-7 flex justify-between items-center">
-          <Button
-            text={"Sign In"}
-            variant={"main"}
-            size={"lg"}
-            endIcon={true}
-          />
-          <p className="text-teal-50 text-lg hover:underline ">Sign Up</p>
+        <div className="w-3/4 h-full m-10 hidden md:block ">
+          <img className="object-cover absolute bottom-0 right-20 -scale-x-100  md:w-100 md:h-110 "
+          src="../../../rb_25585 2.png"/>
         </div>
       </div>
-    </div>
+    </div>  
   );
 };
