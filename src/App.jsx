0702fbb5326c23011e1d876bdb7edddd7483f034
ayushagarwalhere <24bme108@nith.ac.in>
2025-02-06 13:57:44 +0530

    jsx fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { GridBg } from "./component/ui/grid-bg";
-//import { SignIn } from "./pages/SignIn/signin";
+import { SignIn } from "./pages/SignIn/signin";
 function App() {
   return (
     <GridBg>
