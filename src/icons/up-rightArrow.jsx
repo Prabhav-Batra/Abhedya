5e908df8323660c6897826e261d9d2dbc05171fc
ayushagarwalhere <24bme108@nith.ac.in>
2025-02-06 13:45:25 +0530

    jsx fixed

--- src/icons/up-rightArrow.jsx
+++ src/icons/up-rightArrow.jsx
@@ -1,22 +1,19 @@
-import { UpRightArrow } from "../../icons/up-rightArrow.jsx";
-
-const variantStyles = {
-  main: "bg-[#242C31] md:text-lg  text-[#f9f9f9] font-medium rounded border-teal-50/10 border hover:bg-teal-900 cursor-pointer transition-all",
-};
-
-const sizeStyles = {
-  sm: "py-1 px-2 text-sm",
-  md: "py-1 px-3 text-sm",
-  lg: "py-1 px-3 text-lg",
-};
-
-export const Button = ({ variant, text, size, endIcon }) => {
-  return (
-    <Button className={${variantStyles[variant]} ${sizeStyles[size]}}>
-      <div className="flex gap-2 items-center">
-        {text} {endIcon && <UpRightArrow />}
-      </div>
-    </Button>
-  );
-};
-
+export const UpRightArrow = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="3"
+        stroke="currentColor"
+        className="w-4 h-4"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+        />
+      </svg>
+    );
+  };
+  
