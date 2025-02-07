5cb0d94a1a7c8d0574b50d7f9e4b07e608738d66
TanmayThakur2209 <ttanmay22@gmail.com>
2025-02-08 01:35:14 +0530

    new

--- src/pages/profilePage.jsx
+++ src/pages/profilePage.jsx
@@ -1,14 +1,28 @@
 export const ProfilePage = ()=>{
     return<>
     
-    <div class="h-screen w-screen mt-10 left-0 bg-[radial-gradient(22.76%_27.78%_at_50%_46.68%,_#048484_0%,_#001A1A_100%)] absolute  ">
-    
-  <div class="w-screen h-screen absolute "><img className="rounded-full w-64 h-64 mx-auto object-cover -mt-10 overflow-auto border-20 border-[#04281e]" src="https://4kwallpapers.com/images/wallpapers/minion-cartoon-blue-2880x1800-18723.jpg" alt="Circular Image" /></div>
-        <img src="https://raw.githubusercontent.com/Prabhav-Batra/Abhedya/refs/heads/anurag/public/rb_25585%202.png" class="w-250 -ml-80 object-cover  " alt="hi"/> 
-        
-
-
-
+    <div class="w-[1440px] h-screen left-0 bg-[radial-gradient(22.76%_27.78%_at_50%_46.68%,_rgba(4,132,132,0.8)_0%,_rgba(0,26,26,0.8)_100%)] overflow-hidden overflow-y-hidden">
+  <div class="w-screen h-screen absolute ">
+    <img className="rounded-full w-40 h-40 mx-auto object-cover -mt-10 overflow-auto border-20 border-[#04281e] opacity-100" src="https://4kwallpapers.com/images/wallpapers/minion-cartoon-blue-2880x1800-18723.jpg"/>
+    <div class="flex justify-center items-center h-32"  >
+  <p class="text-5xl font-sans font-extrabold bg-gradient-to-b from-green-700 to-green-950 text-transparent bg-clip-text drop-shadow-[0_1px_0px_rgba(0,0,0,1)]" >PLAYER NAME</p>
+</div>
+<div class="flex justify-center text-xl font-sans text-green font-medium -mt-6"><p class="text-[#8e9291]">ABHEDYA ID</p></div>
+            <div class="flex justify-center items-center mx-auto text-[20px] font-sans text-green font-medium mt-5 border-8 border-[#04281e] w-90 h-12 rounded-3xl bg-[#144738]"><p class="text-[#dbe0de]">Position on Leaderboard </p></div>
+            <div class="flex justify-center items-center mx-auto text-[20px] font-sans text-green font-medium mt-5 border-8 border-[#04281e] w-90 h-12 rounded-3xl bg-[#144738]"><p class="text-[#dbe0de]">View Leaderboard</p></div>
+        <div class="flex space-x-0 justify-center mt-10">
+        <div class="mt-10 bg-gradient-to-b from-[#002814] to-[#03917e] justify-center items-center  w-40 h-40"><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100" src="https://dudububushop.com/cdn/shop/articles/Screenshot_2024-10-16_at_9.25.05.png?v=1729042155&width=1100"/></div>
+        <div class="mt-0 bg-gradient-to-b from-[#002814] to-[#03917e] justify-center items-center  w-40 h-50"><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100" src="https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2022/06/befunky-collagery5rwdeqwefdwad-16-1654258234.jpg"/></div>
+        <div class="mt-10 bg-gradient-to-b from-[#002814] to-[#03917e] justify-center items-center  w-40 h-40"><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100" src="https://media.tenor.com/KhLwVytb6MsAAAAe/dudu-dudu-bear.png"/></div>
+      </div>
+      
+      
+      </div>
+        <img src="https://raw.githubusercontent.com/Prabhav-Batra/Abhedya/refs/heads/anurag/public/rb_25585%202.png" class="w-250 -ml-80 object-cover"/> 
+        <div class="absolute bg-white w-500 h-30 -ml-200 -mt-100 -rotate-45 opacity-40"></div>
+        <div class="absolute w-[1440px] h-[1024px] bg-[linear-gradient(269.87deg,_rgba(0,31,31,0.9)_0.08%,_rgba(69,116,115,0.24)_99.88%)] 
+            shadow-md rounded border-none box-border">
+</div>
 </div>
 
 
