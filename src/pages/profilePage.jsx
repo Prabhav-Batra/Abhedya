dd3e4efa9e30a5f269c986ca98ae9954120a3b86
TanmayThakur2209 <ttanmay22@gmail.com>
2025-02-09 02:21:47 +0530

    final

--- src/pages/profilePage.jsx
+++ src/pages/profilePage.jsx
@@ -3,28 +3,26 @@ import "./animate.css"
 export const ProfilePage = ()=>{
     return<>
     
-    <div class="w-[1440px] h-screen left-0 bg-[radial-gradient(22.76%_27.78%_at_50%_46.68%,_rgba(4,132,132,0.8)_0%,_rgba(0,26,26,0.8)_100%)] overflow-hidden overflow-y-hidden">
-  <div class="w-screen h-screen absolute ">
-    <img className="rounded-full w-40 h-40 mx-auto object-cover -mt-10 overflow-auto border-20 border-[#04281e] opacity-100" src="https://4kwallpapers.com/images/wallpapers/minion-cartoon-blue-2880x1800-18723.jpg"/>
-    <div class="flex justify-center items-center h-32"  >
-  <p class="text-5xl font-sans font-extrabold bg-gradient-to-b from-green-700 to-green-950 text-transparent bg-clip-text drop-shadow-[0_1px_0px_rgba(0,0,0,1)]" >PLAYER NAME</p>
+    <div class="w-screen h-screen mr-500px left-0 bg-[radial-gradient(22.76%_27.78%_at_50%_46.68%,_rgba(4,132,132,0.8)_0%,_rgba(0,26,26,0.8)_100%)] flex">
+  <div class="w-screen h-screen absolute z-10 ml-50px ">
+    <img className="rounded-full w-50 h-50 mx-auto object-cover -mt-10 overflow-auto border-20 border-[#04281e] opacity-100 hover:scale-105 duration-300" id="box" src="https://4kwallpapers.com/images/wallpapers/minion-cartoon-blue-2880x1800-18723.jpg"/>
+    <div class="flex justify-center items-center h-25"  >
+  <p class="text-5xl font-sans font-extrabold bg-gradient-to-b from-[#FFFFFF] to-[#0498AE] text-transparent bg-clip-text drop-shadow-[0_1px_0px_rgba(0,0,0,1)] hover:scale-105 duration-300" >PLAYER NAME</p>
 </div>
-<div id="ido" class="flex justify-center text-xl font-sans text-green font-medium -mt-6"><p class="text-[#8e9291]">ABHEDYA ID</p></div>
-            <div  class="flex justify-center items-center mx-auto text-[20px] font-sans text-green font-medium mt-5 w-90 h-12 rounded-3xl bg-gradient-to-b from-[#04281e] to-[#0a4543] p-2 "><div class=" bg-[#0C3136]  flex w-90 rounded-3xl border-0 items-center justify-center mx-auto text-[#ffffff] ">Position on Leaderboard </div></div>
-            <div class="flex justify-center items-center mx-auto text-[20px] font-sans text-green font-medium mt-5 w-90 h-12 rounded-3xl bg-gradient-to-b from-[#04281e] to-[#0a4543] p-2"><div class=" bg-[#0C3136]  flex w-90 rounded-3xl border-0 items-center justify-center mx-auto text-[#ffffff] ">View Leaderboard </div></div>
+<div id="ido" class="flex justify-center text-xl font-sans text-green font-medium -mt-6"><p class="text-[#8e9291] hover:scale-105 duration-300">ABHEDYA ID</p></div>
+            <div id="box" class="flex justify-center items-center mx-auto text-[20px] font-sans text-green font-medium mt-5 w-90 h-12 rounded-3xl bg-gradient-to-b from-[#04281e] to-[#0a4543] p-2"><div class=" bg-[#0C3136] flex w-89 h-11 rounded-3xl items-center justify-center mx-auto text-[#ffffff] absolute ">Position on Leaderboard</div></div>
+            <div id="box" class="flex justify-center items-center mx-auto text-[20px] font-sans text-green font-medium mt-5 w-90 h-12 rounded-3xl bg-gradient-to-b from-[#04281e] to-[#0a4543] p-2"><div class=" bg-[#0C3136] flex w-89 h-11 rounded-3xl items-center justify-center mx-auto text-[#ffffff] absolute">View Leaderboard </div></div>
         <div class="flex space-x-0 justify-center mt-10">
-        <div class="mt-20 bg-gradient-to-b from-[#010D0F] to-[#065460] justify-center flex w-40 h-30 relative"><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100 z-10 absolute" src="https://dudububushop.com/cdn/shop/articles/Screenshot_2024-10-16_at_9.25.05.png?v=1729042155&width=1100"/><div className="absolute rounded-xl w-40 h-10 bg-gradient-to-b from-[#1A9B95] to-[#172D48] "></div></div>
-        <div class="mt-10 bg-gradient-to-b from-[#010D0F] to-[#065460] justify-center flex w-40 h-40"><img src="https://static.vecteezy.com/system/resources/thumbnails/024/661/403/small_2x/simple-golden-crown-icon-vector-illustration-png.png" alt="Crown" class="flex absolute w-auto h-8 -mt-18 mx-auto justify center items-center object-cover rounded-lg shadow-lg transition-transform duration-300 hover:scale-110 hover:shadow-2xl" /><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100 z-10" src="https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2022/06/befunky-collagery5rwdeqwefdwad-16-1654258234.jpg"/><div className="absolute rounded-xl w-40 h-10 bg-gradient-to-b from-[#1A9B95] to-[#172D48] "></div></div>
-        <div class="mt-20 bg-gradient-to-b from-[#010D0F] to-[#065460] justify-center flex w-40 h-30"><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100 z-10" src="https://media.tenor.com/KhLwVytb6MsAAAAe/dudu-dudu-bear.png"/><div className="absolute rounded-xl w-40 h-10 bg-gradient-to-b from-[#1A9B95] to-[#172D48] "></div></div>
+        <div class="mt-20 bg-gradient-to-b from-[#010D0F] to-[#065460] justify-center flex w-40 h-30 rounded-xl"><img id="box" className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100 z-10 absolute hover:scale-105 duration-300" src="https://dudububushop.com/cdn/shop/articles/Screenshot_2024-10-16_at_9.25.05.png?v=1729042155&width=1100"/><div className="absolute rounded-xl w-40 h-10 bg-gradient-to-b from-[#1A9B95] to-[#172D48] "></div></div>
+        <div class="mt-10 bg-gradient-to-b from-[#010D0F] to-[#065460] justify-center flex w-40 h-40 rounded-xl"><img src="https://static.vecteezy.com/system/resources/thumbnails/024/661/403/small_2x/simple-golden-crown-icon-vector-illustration-png.png" alt="Crown" className="flex absolute w-auto h-8 -mt-18 mx-auto justify center items-center object-cover rounded-lg shadow-lg transition-transform duration-300 hover:scale-110 hover:shadow-2xl" /><img id="box" className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 hover:scale-105 duration-300 border-[#04281e] opacity-100 z-10" src="https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2022/06/befunky-collagery5rwdeqwefdwad-16-1654258234.jpg"/><div className="absolute rounded-xl w-40 h-10 bg-gradient-to-b from-[#1A9B95] to-[#172D48] "></div></div>
+        <div class="mt-20 bg-gradient-to-b from-[#010D0F] to-[#065460] justify-center flex w-40 h-30 rounded-xl"><img id="box" className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100 z-10 hover:scale-105 duration-300" src="https://media.tenor.com/KhLwVytb6MsAAAAe/dudu-dudu-bear.png"/><div className="absolute rounded-xl w-40 h-10 bg-gradient-to-b from-[#1A9B95] to-[#172D48] "></div></div>
       </div>
       
       
       </div>
-        <img src="https://raw.githubusercontent.com/Prabhav-Batra/Abhedya/refs/heads/anurag/public/rb_25585%202.png" class="w-250 -ml-80 object-cover"/> 
-        <div class="absolute bg-white w-500 h-30 -ml-200 -mt-100 -rotate-45 opacity-40"></div>
-        <div class="absolute w-[1440px] h-[1024px] bg-[linear-gradient(269.87deg,_rgba(0,31,31,0.9)_0.08%,_rgba(69,116,115,0.24)_99.88%)] 
-            shadow-md rounded border-none box-border">
-</div>
+        <img src="https://raw.githubusercontent.com/Prabhav-Batra/Abhedya/refs/heads/Tanmay/src/pages/rb_25585.png" class="-ml-40 z-10 scale-170 mt-50 object-cover overflow-auto hover:ml-0.5 duration-3000"/> 
+        <div class="absolute bg-white w-500 h-30 -ml-180 -rotate-45 opacity-40 z-0"></div>
+        {/* <div class="absolute w-screen h-screen  bg-[linear-gradient(269.87deg,_rgba(0,31,31,0.8)_0.08%,_rgba(69,116,115,0.24)_99.88%)] shadow-md rounded border-none box-border"></div> */}
 </div>
 
 
