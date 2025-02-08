afd0f60750bf3ec749b5fbe8d6114225d0cc162f
TanmayThakur2209 <ttanmay22@gmail.com>
2025-02-08 22:18:10 +0530

    new

--- src/pages/profilePage.jsx
+++ src/pages/profilePage.jsx
@@ -1,3 +1,5 @@
+import "./profile_page.css"
+import "./animate.css"
 export const ProfilePage = ()=>{
     return<>
     
@@ -7,13 +9,13 @@ export const ProfilePage = ()=>{
     <div class="flex justify-center items-center h-32"  >
   <p class="text-5xl font-sans font-extrabold bg-gradient-to-b from-green-700 to-green-950 text-transparent bg-clip-text drop-shadow-[0_1px_0px_rgba(0,0,0,1)]" >PLAYER NAME</p>
 </div>
-<div class="flex justify-center text-xl font-sans text-green font-medium -mt-6"><p class="text-[#8e9291]">ABHEDYA ID</p></div>
-            <div class="flex justify-center items-center mx-auto text-[20px] font-sans text-green font-medium mt-5 border-8 border-[#04281e] w-90 h-12 rounded-3xl bg-[#144738]"><p class="text-[#dbe0de]">Position on Leaderboard </p></div>
-            <div class="flex justify-center items-center mx-auto text-[20px] font-sans text-green font-medium mt-5 border-8 border-[#04281e] w-90 h-12 rounded-3xl bg-[#144738]"><p class="text-[#dbe0de]">View Leaderboard</p></div>
+<div id="ido" class="flex justify-center text-xl font-sans text-green font-medium -mt-6"><p class="text-[#8e9291]">ABHEDYA ID</p></div>
+            <div  class="flex justify-center items-center mx-auto text-[20px] font-sans text-green font-medium mt-5 w-90 h-12 rounded-3xl bg-gradient-to-b from-[#04281e] to-[#0a4543] p-2 "><div class=" bg-[#0C3136]  flex w-90 rounded-3xl border-0 items-center justify-center mx-auto text-[#ffffff] ">Position on Leaderboard </div></div>
+            <div class="flex justify-center items-center mx-auto text-[20px] font-sans text-green font-medium mt-5 w-90 h-12 rounded-3xl bg-gradient-to-b from-[#04281e] to-[#0a4543] p-2"><div class=" bg-[#0C3136]  flex w-90 rounded-3xl border-0 items-center justify-center mx-auto text-[#ffffff] ">View Leaderboard </div></div>
         <div class="flex space-x-0 justify-center mt-10">
-        <div class="mt-10 bg-gradient-to-b from-[#002814] to-[#03917e] justify-center items-center  w-40 h-40"><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100" src="https://dudububushop.com/cdn/shop/articles/Screenshot_2024-10-16_at_9.25.05.png?v=1729042155&width=1100"/></div>
-        <div class="mt-0 bg-gradient-to-b from-[#002814] to-[#03917e] justify-center items-center  w-40 h-50"><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100" src="https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2022/06/befunky-collagery5rwdeqwefdwad-16-1654258234.jpg"/></div>
-        <div class="mt-10 bg-gradient-to-b from-[#002814] to-[#03917e] justify-center items-center  w-40 h-40"><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100" src="https://media.tenor.com/KhLwVytb6MsAAAAe/dudu-dudu-bear.png"/></div>
+        <div class="mt-20 bg-gradient-to-b from-[#010D0F] to-[#065460] justify-center flex w-40 h-30 relative"><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100 z-10 absolute" src="https://dudububushop.com/cdn/shop/articles/Screenshot_2024-10-16_at_9.25.05.png?v=1729042155&width=1100"/><div className="absolute rounded-xl w-40 h-10 bg-gradient-to-b from-[#1A9B95] to-[#172D48] "></div></div>
+        <div class="mt-10 bg-gradient-to-b from-[#010D0F] to-[#065460] justify-center flex w-40 h-40"><img src="https://static.vecteezy.com/system/resources/thumbnails/024/661/403/small_2x/simple-golden-crown-icon-vector-illustration-png.png" alt="Crown" class="flex absolute w-auto h-8 -mt-18 mx-auto justify center items-center object-cover rounded-lg shadow-lg transition-transform duration-300 hover:scale-110 hover:shadow-2xl" /><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100 z-10" src="https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2022/06/befunky-collagery5rwdeqwefdwad-16-1654258234.jpg"/><div className="absolute rounded-xl w-40 h-10 bg-gradient-to-b from-[#1A9B95] to-[#172D48] "></div></div>
+        <div class="mt-20 bg-gradient-to-b from-[#010D0F] to-[#065460] justify-center flex w-40 h-30"><img className="rounded-full w-20 h-20 mx-auto object-cover -mt-10 overflow-auto border-5 border-[#04281e] opacity-100 z-10" src="https://media.tenor.com/KhLwVytb6MsAAAAe/dudu-dudu-bear.png"/><div className="absolute rounded-xl w-40 h-10 bg-gradient-to-b from-[#1A9B95] to-[#172D48] "></div></div>
       </div>
       
       
