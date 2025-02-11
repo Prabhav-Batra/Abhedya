eaab3ff9734178d21a1b4a7b194581b2aa86c743
Azfurk <24bms027@nith.ac.in>
2025-02-11 21:38:43 +0530

    added a bit of responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { GridBg } from "./components/gridbg";
 import {GamePage} from "./pages/gamepage";
+import { Navbar } from "./components/navbar";
 function App() {
   return (
     <GridBg>
       <GamePage/>
+      
     </GridBg>
   );
 }
