14fe07a7cd2d2c9dda606130f628d8405b59bd5a
Anurag Singh <anuragsingh2313@gmail.com>
2025-03-11 09:50:45 +0530

    minor fixes

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -5,7 +5,7 @@ export const SignIn = () => {
   return (
     <div className="flex justify-center items-center ">
       <div className="flex lg:w-1/2 justify-center items-center min-h-screen p-5">
-        <div className="bg-slate-600/20 rounded-xl py-8 px-10 backdrop-blur-[3px] border-2 border-teal-800 h-fit">
+        <div className="bg-slate-600/20 lg:w-1/2 rounded-xl py-10 px-10 backdrop-blur-[3px] border-2 border-teal-800 h-fit">
           <LinearGradientText
             text={"Sign In"}
             subtitlePreset={false}
@@ -15,8 +15,13 @@ export const SignIn = () => {
             <Input type="text" placeholder="Email" />
             <Input type="password" placeholder="Password" />
           </div>
-          <div className="mt-7 flex justify-between items-center">
-            <Button text={"Sign In"} variant={"main"} size={"lg"} />
+          <div className="mt-8 flex justify-between items-center">
+            <Button
+              text={"Sign In"}
+              variant={"blackBg"}
+              animation={true}
+              size={"lg"}
+            />
             <p className="text-teal-50 font-medium md:text-xl text-lg hover:underline whitespace-nowrap ">
               Sign Up
             </p>
@@ -32,4 +37,3 @@ export const SignIn = () => {
     </div>
   );
 };
-
--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -5,7 +5,7 @@ export const SignUp = () => {
   return (
     <div className="flex justify-center items-center ">
       <div className="flex lg:w-1/2 justify-center items-center min-h-screen p-5">
-        <div className="bg-slate-600/20  rounded-xl py-8 px-10 backdrop-blur-[3px] border-2 border-teal-800 h-fit">
+        <div className="bg-slate-600/20 lg:w-1/2 rounded-xl py-8 px-12 backdrop-blur-[3px] border-2 border-teal-800 h-fit">
           <LinearGradientText
             text={"Sign Up"}
             subtitlePreset={false}
@@ -16,12 +16,12 @@ export const SignUp = () => {
             <Input type="password" placeholder="Password" />
             <Input type="password" placeholder="Confirm Password" />
           </div>
-          <div className="mt-7 flex justify-between items-center">
+          <div className="mt-8 flex justify-between items-center">
             <Button
               text={"Sign Up"}
-              variant={"main"}
+              variant={"blackBg"}
               size={"lg"}
-              endIcon={true}
+              animation={true}
             />
             <p className="text-teal-50 font-medium md:text-xl text-lg hover:underline whitespace-nowrap ">
               Sign In
