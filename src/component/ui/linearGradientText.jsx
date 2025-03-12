d15194ce8f18f1f2ee411740e48fd5bf092d0540
Anurag Singh <anuragsingh2313@gmail.com>
2025-03-12 12:07:26 +0530

    minor changes

--- src/component/ui/linearGradientText.jsx
+++ src/component/ui/linearGradientText.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 
 const sizeStyles = {
   lg: " text-6xl sm:text-[15vw]  ",
-  base: "md:text-[8vw] text-[15vw] text-center pb-5",
+  base: "md:text-[8vw] text-[15vw] text-center ",
   md: "text-6xl text-center pb-5",
 };
 
