68906f3016c8478e40c0cdf70b821fc201fec67f
Prabhav-Batra <prabhavbatra4@gmail.com>
2025-03-14 11:52:53 +0530

    leaderboard adaptive

--- src/pages/leaderboard.jsx
+++ src/pages/leaderboard.jsx
@@ -13,13 +13,13 @@ export const Leaderboard = () => {
   return (
     <body className="">
       <div className="min-h-screen min-w-screen ">
-        <p1 className="text-base sm:text-2xl md:text-4xl lg:text-7xl xl:text-9xl border-l-8 bg-gradient-to-b from-teal-900 to-transparent text-transparent bg-clip-text mx-auto flex justify-center mt-30 font-extrabold">
+        <p1 className="text-5xl sm:text-4xl md:text-4xl lg:text-7xl xl:text-9xl border-l-8 bg-gradient-to-b from-teal-900 to-transparent text-transparent bg-clip-text mx-auto flex justify-center mt-30 font-extrabold">
           LEADERBOARD
         </p1>
         <div className="bg-[#1E1E1E] rounded-xl  min-h-200 mx-50 p-10">
           <div className="flex flex-wrap justify-center items-end space-x-10 mb-8">
-            <div className="bg-teal-900 p-4 w-32 h-40 rounded-lg flex flex-col items-center shadow-md">
-              <div className="w-20 h-20 bg-gray-800 rounded-full mb-2"></div>
+            <div className="bg-teal-900 p-4 w-32 h-40 sm:w-22 sm:h-30 rounded-lg flex flex-col items-center shadow-md">
+              <div className="w-20 h-20 bg-gray-800 rounded-full mb-2 "></div>
               <p className="text-white font-semibold text-center">
                 NAME
                 <br />
@@ -27,7 +27,7 @@ export const Leaderboard = () => {
               </p>
             </div>
 
-            <div className="bg-teal-900 p-6 w-40 h-48 rounded-lg flex flex-col items-center shadow-md">
+            <div className="bg-teal-900 p-6 w-40 h-48 sm:w-30 sm:h-38 rounded-lg flex flex-col items-center shadow-md">
               <div className="w-24 h-24 bg-gray-800 rounded-full mb-2"></div>
               <p className="text-white font-semibold text-center">
                 NAME
@@ -36,7 +36,7 @@ export const Leaderboard = () => {
               </p>
             </div>
 
-            <div className="bg-teal-900 p-4 w-28 h-36 rounded-lg flex flex-col items-center shadow-md">
+            <div className="bg-teal-900 p-4 w-28 h-36 sm:w-18 sm:h-26 rounded-lg flex flex-col items-center shadow-md">
               <div className="w-16 h-16 bg-gray-800 rounded-full mb-2"></div>
               <p className="text-white font-semibold text-center">
                 NAME
